fe9c0f4bc9888b043cdf96fe0a4b97f061e8c26f
oumar <oumarfilston@gmail.com>
2021-03-24 00:45:08 -0700

    first commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
  var {mongoose} = require('./db/mongoose');
  var {Todo} = require('./models/todo');
  var {User} = require('./models/user');
+ var {ObjectID} = require('mongodb');
 
 var app = express();
 app.use(bodyParser.json());
@@ -16,6 +17,29 @@ app.post('/todos',(req,res)=>{
         res.status(400).csend(e);
     });
 });
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos})
+    },(e)=>{
+        res.status(400).send(e);
+    })
+});
+//get a single todo with id
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id)
+    .then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+})
 app.listen(3000,()=>{
     console.log('Started on port 3000');
 })
