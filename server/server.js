1b380df5e97a542a9d1a5a348e2c41885da3155a
oumar <oumarfilston@gmail.com>
2021-03-24 09:14:25 -0700

    Add post user api

--- server/server.js
+++ server/server.js
@@ -78,6 +78,16 @@ app.patch('/todos/:id',(req,res)=>{
         res.status(400).send();
     });
 });
+//Post /Users
+app.post('/users',(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+    var user = new User(body);
+    user.save().then((user)=>{
+        res.send(user);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+});
 
 app.listen(port,()=>{
     console.log(`Started  up at port ${port}`);
