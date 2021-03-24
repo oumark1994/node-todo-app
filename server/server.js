cb9755dd15899e17aad33278f9a773a73e26bc62
oumar <oumarfilston@gmail.com>
2021-03-24 05:44:12 -0700

    Add delete and patch api

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get('/todos/:id',(req,res)=>{
     })
 })
 //delete todo
-app.delete('/todo/:id',(req,res)=>{
+app.delete('/todos/:id',(req,res)=>{
     var id = req.params.id;
     if(!ObjectID.isValid(id)){
         return res.status(404).send();
