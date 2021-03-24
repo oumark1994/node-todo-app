5c99175586c6de88fb33428d8c5afcca538d14ec
oumar <oumarfilston@gmail.com>
2021-03-24 05:40:21 -0700

    Add delete and patch api

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
  var express = require('express');
+ const _ = require('lodash');
  var bodyParser = require('body-parser');
  var {mongoose} = require('./db/mongoose');
  var {Todo} = require('./models/todo');
  var {User} = require('./models/user');
  var {ObjectID} = require('mongodb');
+const { ReplSet } = require('mongodb');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -41,6 +43,43 @@ app.get('/todos/:id',(req,res)=>{
         res.status(400).send();n
     })
 })
+//delete todo
+app.delete('/todo/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e)=>{
+    res.status(400).send();
+    })
+});
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date.getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findOneAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+});
+
 app.listen(port,()=>{
     console.log(`Started  up at port ${port}`);
 })
