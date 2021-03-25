1a27d585ca1e33314ffd5fb7578d5f4e7b0797df
oumar <oumarfilston@gmail.com>
2021-03-25 08:22:17 -0700

    hash password before store to db

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var validator = require('validator');
 var jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 var UserSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -63,5 +64,19 @@ UserSchema.statics.findByToken = function(token){
 
     });
 };
+UserSchema.pre('save',function(next){
+    var user  = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                user.password = hash;
+                next();
+            });
+        });
+
+    }else{
+        next();
+    }
+})
 var User  = mongoose.model('User',UserSchema);
 module.exports = {User};
