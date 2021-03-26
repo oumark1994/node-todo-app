1cca3fa8e28e653d43863d751644ebf3bea3549d
oumar <oumarfilston@gmail.com>
2021-03-26 04:14:56 -0700

    make route private

--- server/server.js
+++ server/server.js
@@ -10,9 +10,10 @@
 var app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
-app.post('/todos',(req,res)=>{
+app.post('/todos', authenticate,(req,res)=>{
     var todo = new Todo({
-        text:req.body.text
+        text:req.body.text,
+        _creator:req.user._id
     });
     todo.save().then((doc)=>{
         res.send(doc);
@@ -21,7 +22,9 @@ app.post('/todos',(req,res)=>{
     });
 });
 app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
+    Todo.find({
+        _creator :req.user._id
+    }).then((todos)=>{
         res.send({todos})
     },(e)=>{
         res.status(400).send(e);
